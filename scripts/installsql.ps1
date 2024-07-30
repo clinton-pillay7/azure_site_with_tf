2b448aa9bdb91450688e808a27363c3e5a1a241d
Clinton <clinton.bitcoin@gmail.com>
2024-07-31 00:05:57 +0200

    refined SQL install and import data script

--- scripts/installsql.ps1
+++ scripts/installsql.ps1
@@ -1,3 +1,64 @@
-$configfile = "C:\users\clintonpillay\Downloads\ConfigurationFile.ini.txt"
-$command = "C:\Users\clintonpillay\Downloads\SQLEXPRADV_x64_ENU\SETUP.EXE /configurationFile=$($configfile) /INDICATEPROGRESS"
-Invoke-Expression -command $command
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$sqlurl =  "https://mypsscripts.blob.core.windows.net/scripts/SQLEXPRADV_x64_ENU.zip" 
+$configfileurl = "https://mypsscripts.blob.core.windows.net/scripts/configurationfile.ini"
+$sqlziplocation = "C:\users\clintonpillay\downloads\SQLEXPRADV_x64_ENU.zip"
+$sqlsourcefolder = "C:\users\clintonpillay\downloads\"
+$sqlconfigfile = "C:\users\clintonpillay\Downloads\configurationfile.ini"
+
+
+Invoke-Webrequest $sqlurl -OutFile $sqlziplocation
+Expand-Archive -Path $sqlziplocation -DestinationPath $sqlsourcefolder 
+Invoke-Webrequest $configfileurl -OutFile $sqlconfigfile
+
+
+
+$sqlcommand = "C:\Users\clintonpillay\Downloads\SQLEXPRADV_x64_ENU\setup.exe /configurationFile=$($sqlconfigfile) /INDICATEPROGRESS"
+Invoke-Expression -command $sqlcommand
+
+
+install-packageprovider -name nuget -minimumversion 2.8.5.201 -force 
+#Register-PSRepository -Default
+
+# Update PowerShellGet and PackageManagement modules
+Install-Module -Name PowerShellGet -Force -Scope CurrentUser -AllowClobber
+Install-Module -Name PackageManagement -Force -Scope CurrentUser -AllowClobber
+Install-Module -Name SqlServer -Force -Scope CurrentUser -AllowClobber
+
+
+
+# Define the SQL Server connection details
+$serverName = "localhost\SQLEXPRESS"
+$username = "sa"
+$password = "YourStrong!Passw0rd"
+
+# Step 1: Create Database
+$sqlCreateDatabase = @"
+CREATE DATABASE mydb
+"@
+Invoke-Sqlcmd -ServerInstance $serverName -Username $username -Password $password -Query $sqlCreateDatabase -TrustServerCertificate
+
+
+# Step 2: Create Table
+$databaseName = "mydb"
+$sqlCreateTable = @"
+CREATE TABLE mytable (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    name NVARCHAR(50),
+    surname NVARCHAR(50)
+)
+"@
+Invoke-Sqlcmd -ServerInstance $serverName -Database $databaseName -Username $username -Password $password -Query $sqlCreateTable -TrustServerCertificate
+
+
+# Step 3: Insert Data
+$serverName = "localhost\SQLEXPRESS"
+$username = "sa"
+$password = "YourStrong!Passw0rd"
+$databaseName = "mydb"
+$sqlInsertCommand = @"
+INSERT INTO mytable (name, surname)
+VALUES ('clinton', 'pillay')
+"@
+
+Invoke-Sqlcmd -ServerInstance $serverName -Database $databaseName -Username $username -Password $password -Query $sqlInsertCommand -TrustServerCertificate
